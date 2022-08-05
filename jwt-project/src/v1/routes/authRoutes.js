cfcc411aba015adf4185b14c1cc25e8df4a12e2c
Roshan Maddumage <roshanma@unicorn-solutions.com>
2022-08-05 22:04:13 +0530

    fixed conflicts

--- jwt-project/src/v1/routes/authRoutes.js
+++ jwt-project/src/v1/routes/authRoutes.js
@@ -8,6 +8,4 @@ router.post('/register', authController.createNewUser);
 
 router.post('/login', authController.findUser);
 
-router.get('/refresh', authController.refreshToken);
-
 module.exports = router;
